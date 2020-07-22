e890f9b178d78e11e9ee4865822c6d517aa21b96
GitHub Action <action@github.com>
2020-07-22 08:40:20 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/zill057/ideapluginopenwithcode/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.zill057.ideapluginopenwithcode.services
+
+import com.github.zill057.ideapluginopenwithcode.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/zill057/ideapluginopenwithcode/services/MyProjectService.kt
+++ src/main/kotlin/com/github/zill057/ideapluginopenwithcode/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.zill057.ideapluginopenwithcode.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.zill057.ideapluginopenwithcode.MyBundle
 
 class MyProjectService(project: Project) {
 
